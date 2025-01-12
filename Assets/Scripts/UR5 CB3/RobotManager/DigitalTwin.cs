2df171cbaf6ca2ac4043e2c013f3d77e8e35936c
Nour Fendri <nourfendri@hotmail.com>
2025-01-12 16:41:03 +0100

    UI Updates and folder restructue

--- Assets/Scripts/UR5 CB3/RobotManager/DigitalTwin.cs
+++ Assets/Scripts/UR5 CB3/RobotManager/DigitalTwin.cs
@@ -6,6 +6,27 @@ public class DigitalTwin : MonoBehaviour
     #region Robot CAD Model Configuration
     private const int NumRobotJoints = 6;
     private ArticulationBody[] _jointArticulationBodies;
+    private ArticulationBody _leftKnuckle;
+    private ArticulationBody _leftInnerKnuckle;
+    private ArticulationBody _leftInnerKnuckleFingerTip;
+    private ArticulationBody _rightKnuckle;
+    private ArticulationBody _rightInnerKnuckle;
+    private ArticulationBody _rightInnerKnuckleFingerTip;
+
+    public GameObject leftKnuckle;
+    public GameObject rightKnuckle;
+    public GameObject leftInnerKnuckle;
+    public GameObject rightInnerKnuckle;
+    public GameObject leftInnerKnuckleFingerTip;
+    public GameObject rightInnerKnuckleFingerTip;
+
+
+    public float leftKnuckleDriveTarget = 0f;
+    public float rightKnuckleDriveTarget = 0f;
+    public float leftInnerKnuckleDriveTarget = 0f;
+    public float rightInnerKnuckleDriveTarget = 0f;
+    public float leftInnerKnuckleFingerTipDriveTarget = 0f;
+    public float rightInnerKnuckleFingerTipDriveTarget = 0f;
     public GameObject UR5Robot;
     #endregion
 
@@ -26,11 +47,37 @@ public class DigitalTwin : MonoBehaviour
             linkName += SourceDestinationPublisher.Ur5LinkNames[i];
             _jointArticulationBodies[i] = UR5Robot.transform.Find(linkName).GetComponent<ArticulationBody>();
         }
+
+        //var leftInnerKnuckleLink = linkName + "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_left_inner_knuckle_link";
+        //var leftKnuckleLink = linkName + "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_left_knuckle_link";
+        ////var leftInnerKnuckleLinkFingerTip = linkName + "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_left_inner_knuckle_link/robotiq_85_left_finger_tip_link";
+
+        //var rightInnerKnuckleLink = linkName + "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_right_inner_knuckle_link";
+        //var rightKnuckleLink = linkName + "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_right_knuckle_link";
+        ////var rightInnerKnuckleLinkFingerTip = "/tool0/robotiq_coupler/robotiq_85_base_link/robotiq_85_right_inner_knuckle_link/robotiq_85_right_finger_tip_link";
+
+        //var rightInnerKnuckleTransform = UR5Robot.transform.Find(rightKnuckleLink);
+        _rightKnuckle = rightKnuckle.GetComponent<ArticulationBody>();
+        _rightInnerKnuckle = rightInnerKnuckle.GetComponent<ArticulationBody>();
+        _rightInnerKnuckleFingerTip = rightInnerKnuckleFingerTip.GetComponent<ArticulationBody>();
+
+        _leftKnuckle = leftKnuckle.GetComponent<ArticulationBody>();
+        _leftInnerKnuckle = leftInnerKnuckle.GetComponent<ArticulationBody>();
+        _leftInnerKnuckleFingerTip = leftInnerKnuckleFingerTip.GetComponent<ArticulationBody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.O)) { 
+            OpenGripper();
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            CloseGripper();
+        }
+
         if (_dataProcessor.Angles == new double[] { 0, 0, 0, 0, 0, 0 }) return;
         MimicRealRobot(_dataProcessor.Angles);
     }
@@ -51,5 +98,54 @@ public class DigitalTwin : MonoBehaviour
             joint1XDrive.target = robotAnglesInDegrees[joint];
             _jointArticulationBodies[joint].xDrive = joint1XDrive;
         }
+    }
+
+    void OpenGripper()
+    {
+        var leftKnuckleDrive = _leftKnuckle.xDrive;
+        var rightKnuckleDrive = _rightKnuckle.xDrive;
+        var leftInnerKnuckleDrive = _leftInnerKnuckleFingerTip.xDrive;
+        var leftInnerKnuckleFingerTipDrive = _leftInnerKnuckle.xDrive;
+        var rightInnerKnuckleDrive = _rightInnerKnuckle.xDrive;
+        var rightInnerKnuckleFingerTipDrive = _rightInnerKnuckleFingerTip.xDrive;
+
+        leftInnerKnuckleDrive.target = 0f;
+        leftKnuckleDrive.target = 0f;
+        rightInnerKnuckleDrive.target = 0f;
+        rightKnuckleDrive.target = 0f;
+        leftInnerKnuckleFingerTipDrive.target = 0f;
+        rightInnerKnuckleFingerTipDrive.target = 0f;
+
+        _leftKnuckle.xDrive = leftKnuckleDrive;
+        _leftInnerKnuckle.xDrive = leftInnerKnuckleDrive;
+        _rightKnuckle.xDrive = rightKnuckleDrive;
+        _rightInnerKnuckle.xDrive = rightInnerKnuckleDrive;
+        _rightInnerKnuckleFingerTip.xDrive = rightInnerKnuckleFingerTipDrive;
+        _leftInnerKnuckleFingerTip.xDrive = leftInnerKnuckleFingerTipDrive;
+    }
+
+    void CloseGripper()
+    {
+        var leftKnuckleDrive = _leftKnuckle.xDrive;
+        var rightKnuckleDrive = _rightKnuckle.xDrive;
+        var leftInnerKnuckleDrive = _leftInnerKnuckle.xDrive;
+        var rightInnerKnuckleDrive = _rightInnerKnuckle.xDrive;
+        var leftInnerKnuckleFingerTipDrive = _leftInnerKnuckle.xDrive;
+        var rightInnerKnuckleFingerTipDrive = _rightInnerKnuckleFingerTip.xDrive;
+
+        leftKnuckleDrive.target = leftKnuckleDriveTarget;
+        leftInnerKnuckleDrive.target = leftInnerKnuckleDriveTarget;
+        rightKnuckleDrive.target = rightKnuckleDriveTarget;
+        rightInnerKnuckleDrive.target = rightInnerKnuckleDriveTarget;
+        leftInnerKnuckleFingerTipDrive.target = leftInnerKnuckleFingerTipDriveTarget;
+        rightInnerKnuckleFingerTipDrive.target = rightInnerKnuckleFingerTipDriveTarget;
+
+
+        _leftKnuckle.xDrive = leftKnuckleDrive;
+        _leftInnerKnuckle.xDrive = leftInnerKnuckleDrive;
+        _rightKnuckle.xDrive = rightKnuckleDrive;
+        _rightInnerKnuckle.xDrive = rightInnerKnuckleDrive;
+        _rightInnerKnuckleFingerTip.xDrive = leftInnerKnuckleFingerTipDrive;
+        _leftInnerKnuckleFingerTip.xDrive = leftInnerKnuckleFingerTipDrive;
     }
 }
